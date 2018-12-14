7643bcdaa66b1646d96f924bfe3378f79843cbc2
Lucymei2018 <lucymei100@yahoo.com>
2018-12-14 14:42:30 -0600

    edit button 2

--- PatrickTest1/Form1.vb
+++ PatrickTest1/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
     Private Sub btnButton1_Click(sender As Object, e As EventArgs) Handles btnButton1.Click
-
+        MsgBox("This is button one, the very First Button!")
     End Sub
 
     Private Sub btnAnotherButton_Click(sender As Object, e As EventArgs) Handles btnAnotherButton.Click
