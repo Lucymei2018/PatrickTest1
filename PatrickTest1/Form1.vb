218a899597687373d4558910f54a1293a8da4354
Lucymei2018 <lucymei100@yahoo.com>
2018-12-14 17:26:47 -0600

    1

--- PatrickTest1/Form1.vb
+++ PatrickTest1/Form1.vb
@@ -4,6 +4,6 @@
     End Sub
 
     Private Sub btnAnotherButton_Click(sender As Object, e As EventArgs) Handles btnAnotherButton.Click
-        MsgBox("This is another button")
+        MsgBox("This is another button,this is for button 2, it looks really much better!!!")
     End Sub
 End Class
